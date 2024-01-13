8195d043e55afcf5f77f825034bd8126100203cd
voyage200 <Hello_cmd@outlook.com>
2024-01-13 23:28:49 +0800

    test

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 分布式 ping 检测计划，让你的小鸡发光发热！
 
-本程序为 https://ping0.cc/ping 的安装脚本，运行此脚本之后，将会让小鸡加入ping0.cc的分布式检测网络。此脚本已经在Debian 11 (amd 64位) 系统上测试通过。
+本程序为 https://ping0.cc/ping 的安装+卸载一体化脚本，运行此脚本之后，可以选择安装/卸载ping0客户端，安装操作将会让小鸡加入ping0.cc的分布式检测网络。此脚本已经在Debian 12 (amd 64位) 系统上测试通过。
 
 本程序会安装一个主程序，安装过程中需要`wget`，安装完成后使用`systemd`守护主程序的运行。除此之外，不会安装其他依赖，不会修改文件，CPU占用极低，内存占用约10MB，平均每天总流量小于 1MB。
 
@@ -10,20 +10,27 @@
 
 changelog:
 2022-03-22：更新至0.2版本，修改systemd配置文件，提高可用性。
+2024-1-13：更新至0.3版本。
 
+## 使用这个一体化脚本
 
-## 一键脚本如下
-```
-wget --no-check-certificate -O ping0.sh https://raw.githubusercontent.com/uselibrary/Ping0Monitor/main/install.sh && chmod +x ping0.sh && bash ping0.sh
+```bash
+wget -O ping0.sh https://raw.githubusercontent.com/uselibrary/Ping0Monitor/main/install.sh && chmod +x ping0.sh 
+
+bash ping0.sh
+
 ```
 
 ### 1. 安装过程：
 
-首先，运行上述一键脚本。
+运行一体化脚本，按提示操作即可。
 
-期间需要输入用户名，此处输入pa.ci为例，用户名最多为8个字符。如果直接回车，则默认用户名为`username`。脚本会自动停用并删除之前的旧版本，再安装新版本。
+预览图
 
-![install](https://raw.githubusercontent.com/uselibrary/Ping0Monitor/main/data/install.jpg)
+期间需要输入用户名，此处输入pa.ci为例，用户名最多为8个字符。如果直接回车，则使用默认用户名：username。
+![install](https://raw.githubusercontent.com/uselibrary/Ping0Monitor/main/data/install1.jpg)
+
+![install](https://raw.githubusercontent.com/uselibrary/Ping0Monitor/main/data/install2.jpg)
 
 ### 2. 安装完成后的查看
 
@@ -33,7 +40,7 @@ wget --no-check-certificate -O ping0.sh https://raw.githubusercontent.com/uselib
 
 程序的运行状态，可以使用以下命令查看：
 
-```
+```bash
 systemctl status ping0.service
 ```
 
@@ -51,26 +58,23 @@ ExecStart=/usr/local/ping0/ping0 username
 
 修改完成后，执行以下命令：
 
-```
+```bash
 systemctl daemon-reload
 systemctl restart ping0.service
 ```
 
 
-
 ### 4. 删除ping0服务
 
-如需要删除ping0的程序和相关服务，依次执行以下命令即可：
+使用一体化脚本即可删除ping0的程序和相关服务，也可以依次执行以下命令：
 
-```
+```bash
 systemctl stop ping0.service
 systemctl disable ping0.service
 rm -rf /usr/local/ping0
 rm -rf /etc/systemd/system/ping0.service
 systemctl daemon-reload
 ```
-
-
 
 ### 5. 系统兼容性
 
--- install.sh
+++ install.sh
@@ -5,96 +5,168 @@ echo "    ################################################"
 echo "    #                                              #"
 echo "    #              ping0.cc分布式检测计划           #"
 echo "    #                 https://ping0.cc             #"
-echo "    #                  Version 0.2                 #"
+echo "    #                  Version 0.3                 #"
 echo "    #            Powered by https://pa.ci          #"
 echo "    ################################################"
 
-# March 21, 2023
+# January 13, 2024
 
-# 检查是否为root用户
-if [ $(id -u) != "0" ]; then
-    echo "错误: 必须使用root权限执行此脚本!"
-    exit 1
-fi
-
-# 检查是否安装过，若有则删除
-if [ -f /usr/local/ping0/ping0 ]; then
-    echo "检测到已安装过ping0，正在删除..."
+function uninstall()
+{
+    echo "开始卸载ping0..."
     systemctl stop ping0.service
     systemctl disable ping0.service
     rm -rf /usr/local/ping0
     rm -rf /etc/systemd/system/ping0.service
     systemctl daemon-reload
-    echo "删除完成!"
-fi
+    echo "卸载流程结束!"
+}
 
+function install_wget()
+{
+    if [ ! -x wget ];then
+        echo "成功探测到所需的 wget 指令！"
+        return 0
+    fi
+    echo "没有探测到所需的 wget 指令，尝试自动安装 wget 软件包..."
 
-# 检查系统是否为RedHat系列或者Debian系列，如果不是则退出
-if [ -f /etc/redhat-release ]; then
-    OS=CentOS
-    PM=yum
-elif [ ! -z "`cat /etc/issue | grep bian`" ]; then
-    OS=Debian
-    PM=apt-get
-elif [ ! -z "`cat /etc/issue | grep Ubuntu`" ]; then
-    OS=Ubuntu
-    PM=apt-get
-else
-    echo "不支持的操作系统!"
+    # 检查系统是否为RedHat系列或者Debian系列，如果不是则退出
+    if [ -f /etc/redhat-release ]; then
+        OS=CentOS
+        PM=yum
+    elif [ ! -z "`cat /etc/issue | grep bian`" ]; then
+        OS=Debian
+        PM=apt-get
+    elif [ ! -z "`cat /etc/issue | grep Ubuntu`" ]; then
+        OS=Ubuntu
+        PM=apt-get
+    else
+        echo "sorry！脚本不支持自动为你的操作系统安装wget！"
+        echo "请手动安装 wget！"
+        exit 1
+    fi
+
+    # 安装依赖 "wget"
+    if [ "$PM" = "yum" ]; then
+        yum install -y wget
+    else
+        apt-get install -y wget
+    fi
+
+}
+
+function install()
+{
+    install_wget
+    echo "开始安装ping0..."
+
+    # 使用 "arch" 命令检查系统架构，x86_64、arm32或arm64
+    if [ `arch` = "x86_64" ]; then
+        ARCH=x86_64
+    elif [ `arch` = "armv7l" ]; then
+        ARCH=arm32
+    elif [ `arch` = "aarch64" ]; then
+        ARCH=arm64
+    else
+        echo "不支持的系统架构!"
+        exit 1
+    fi
+
+    # 新建目录 /usr/local/ping0，并使用wget下载安装文件
+    mkdir -p /usr/local/ping0
+    echo "正在下载主程序..."
+    # x86_64: https://ping0.cc/data/ping0
+    # arm32: https://ping0.cc/data/ping0-arm
+    # arm64: https://ping0.cc/data/ping0-arm64
+    if [ "$ARCH" = "x86_64" ]; then
+        wget -qO- https://ping0.cc/data/ping0 > /usr/local/ping0/ping0
+    elif [ "$ARCH" = "arm32" ]; then
+        wget -qO- https://ping0.cc/data/ping0-arm > /usr/local/ping0/ping0
+    elif [ "$ARCH" = "arm64" ]; then
+        wget -qO- https://ping0.cc/data/ping0-arm64 > /usr/local/ping0/ping0
+    fi
+
+    # 给安装文件添加执行权限
+    chmod +x /usr/local/ping0/ping0
+
+    echo "主程序安装完毕! 开始配置 ping0 systemd 单元文件！"
+
+    echo "
+[Unit]
+Description=Ping0 Monitor Service
+Documentation=https://ping0.cc/ping
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/usr/local/ping0/ping0 username 
+Restart=always
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+" > /etc/systemd/system/ping0.service
+
+    # 要求用户输入用户名
+    read -p "请输入贡献者名: " USERNAME
+    # 如果用户名为空，则使用ping0作为用户名
+    # 将ping0.service中的用户名替换为用户输入的用户名
+    if [ "$USERNAME" != "" ]; then
+        sed -i "s/username/$USERNAME/g" /etc/systemd/system/ping0.service
+    fi
+
+    # 启用systemd管理，并启用ping0服务
+    systemctl daemon-reload
+    systemctl enable ping0.service
+    systemctl start ping0.service
+
+    echo "ping0 systemd 单元文件配置完毕！已启动ping0服务！"
+    echo "安装流程结束！"
+}
+
+# 检查是否为root用户
+if [ $(id -u) != "0" ]; then
+    echo "错误: 需要使用root权限执行此脚本!"
     exit 1
 fi
 
-
-# 安装依赖 "wget"
-if [ "$PM" = "yum" ]; then
-    yum install -y wget
+echo "欢迎来到 ping0 维护脚本！"
+# 检查是否安装过，若有则提供卸载重装选项，没有则提供安装选项
+if [ -f /usr/local/ping0/ping0 ]; then
+    echo "探测到系统中已存在ping0，如何处理？"
+    echo "1.卸载ping0  2.重装最新版ping0  3.退出脚本"
+    read -p "请选择：" choice
+    case $choice in
+        1)
+            uninstall
+            ;;
+        2)
+            echo "重新安装ping0..."
+            uninstall
+            install
+            ;;
+        3)
+            exit 0
+            ;;
+        *)
+            echo "输入错误!"
+            ;;
+    esac
 else
-    apt-get install -y wget
+    echo "请选择你要进行的操作："
+    echo "1.安装ping0  2.退出脚本"
+    read -p "请选择：" choice
+    case $choice in
+        1)
+            install
+            ;;
+        2)
+            exit 0
+            ;;
+        *)
+            echo "输入错误!"
+            ;;
+    esac
 fi
 
-# 使用 "arch" 命令检查系统架构，x86_64、arm32或arm64
-if [ `arch` = "x86_64" ]; then
-    ARCH=x86_64
-elif [ `arch` = "armv7l" ]; then
-    ARCH=arm32
-elif [ `arch` = "aarch64" ]; then
-    ARCH=arm64
-else
-    echo "不支持的系统架构!"
-    exit 1
-fi
-
-# 新建目录 /usr/local/ping0，并使用wget下载安装文件
-mkdir -p /usr/local/ping0
-# x86_64: https://ping0.cc/data/ping0
-# arm32: https://ping0.cc/data/ping0-arm
-# arm64: https://ping0.cc/data/ping0-arm64
-if [ "$ARCH" = "x86_64" ]; then
-    wget --no-check-certificate -qO- https://ping0.cc/data/ping0 > /usr/local/ping0/ping0
-elif [ "$ARCH" = "arm32" ]; then
-    wget --no-check-certificate -qO- https://ping0.cc/data/ping0-arm > /usr/local/ping0/ping0
-elif [ "$ARCH" = "arm64" ]; then
-    wget --no-check-certificate -qO- https://ping0.cc/data/ping0-arm64 > /usr/local/ping0/ping0
-fi
-
-echo "主文件安装完成!"
-
-# 给安装文件添加执行权限
-chmod +x /usr/local/ping0/ping0
-
-# 下载systemd管理文件到/etc/systemd/system/ping0.service
-wget --no-check-certificate -qO- https://raw.githubusercontent.com/uselibrary/Ping0Monitor/main/ping0.service > /etc/systemd/system/ping0.service
-
-# 要求用户输入用户名
-read -p "请输入用户名: " USERNAME
-# 如果用户名为空，则使用ping0作为用户名
-# 将ping0.service中的用户名替换为用户输入的用户名
-if [ "$USERNAME" != "" ]; then
-    sed -i "s/username/$USERNAME/g" /etc/systemd/system/ping0.service
-fi
-
-# 启用systemd管理，并启用ping0服务
-systemctl daemon-reload
-systemctl enable ping0.service
-systemctl start ping0.service
 
